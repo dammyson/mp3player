a9117f47c6a57ed4161adcb67a20508c48809a4b
dammyson <voecmanager@yahoo.com>
2018-05-26 16:10:06 +0100

    Add files via upload

---
+++ main/java/com/ofidy/mp3player/PlaybackStatus.java
@@ -0,0 +1,10 @@
+package com.ofidy.mp3player;
+
+/**
+ * Created by Damilola on 5/24/2018.
+ */
+
+public enum PlaybackStatus {
+    PLAYING,
+    PAUSED
+}
---
+++ main/java/com/ofidy/mp3player/onItemClickListener.java
@@ -0,0 +1,11 @@
+package com.ofidy.mp3player;
+
+import android.view.View;
+
+/**
+ * Created by Damilola on 5/24/2018.
+ */
+
+public interface onItemClickListener {
+    public void onClick(View view, int index);
+}
